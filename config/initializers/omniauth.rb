08356c58936d09024e8a2564b6db0cf6c3f3b319
Timo Herttua <timo.herttua@efekta.fi>
2014-10-27 16:22:58 +0000

    env variables for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-
-  if !Rails.application.config.omniauth_key || !Rails.application.config.omniauth_secret
-    raise 'missing Omniauth key & secret variables for Twitter .. setup as its own initializer and remember to gitignore!'
-  end
-
-  provider :twitter, Rails.application.config.omniauth_key, Rails.application.config.omniauth_secret
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
